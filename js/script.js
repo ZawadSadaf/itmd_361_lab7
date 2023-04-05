45d86a36d4fb32e9cf42df5eada5df1859afe076
Zawad Sadaf <ssadaf@hawk.iit.edu>
2023-04-04 21:24:11 -0500

    changes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function init(){
+//add your javascrip between these two lines of code
+var button = document.getElementById('entrybutton');
+  
+function showMeText(){
+  var textbox = document.getElementById('entryinput');
+  document.getElementById('textoutput').innerHTML = textbox.value;
+  alert("S M Zawad Bin Zaki Sadaf: " + textbox.value);
+}
+
+button.addEventListener('click', showMeText);
+}
+
+window.addEventListener('load', init);
+
+
+
+  
